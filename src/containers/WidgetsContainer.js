f5a581e63026d8eba2d7d5bb42647497b99a79d3
maksyuk vadim <maksyukvadim@gmail.com>
2017-07-21 16:57:42 +0300

    parse weather by date

--- src/containers/WidgetsContainer.js
+++ src/containers/WidgetsContainer.js
@@ -7,12 +7,33 @@ import WidgetsWeather from "../components/WidgetsWeather/WidgetsWeather";
 
 @connect((state) => ({towns: state.weather.towns}), weatherActions)
 class WidgetsContainer extends Component {
-    render() {
-        const { list } = this.props.towns;
 
+    groupData(list) {
+        let enptArr = {};
+        let data = list[0].dt_txt.split(' ')[0].replace(/-/g,'d');
+
+        list.forEach((item) => {
+            if(item.dt_txt.split(' ')[0].replace(/-/g,'d') !== data) {
+                data = item.dt_txt.split(' ')[0].replace(/-/g,'d');
+            }               
+
+            if(enptArr[data]) {
+                enptArr[data].push(item);
+            } else {
+                enptArr[data] = [];
+                enptArr[data].push(item);
+            }
+            console.log(enptArr);
+        });
+        return enptArr;
+    }
+
+    render() {       
+        const { list } = this.props.towns;
+        console.log(list.length > 0 && Object.values(this.groupData(list)));
         return (
             <div>
-                {list.map((town) => <WidgetsWeather weather={town}/>)}               
+                {list.map((town) => <WidgetsWeather key={town.dt} weather={town}/>)}               
             </div>
         );
     }
