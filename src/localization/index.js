d9de24ddfea1c190d41289d60aa65667a9d9df8d
Vadym Maksyuk <maksyukvadim@gmail.com>
2017-09-09 14:09:07 +0300

    add localization

---
+++ src/localization/index.js
@@ -0,0 +1,32 @@
+import LocalizedStrings from 'react-localization';
+
+export default new LocalizedStrings({
+    en:{
+      month01:"jan",
+      month02:"feb",
+      month03:"mar",
+      month04:"apr",
+      month05:"may",
+      month06:"jun",
+      month07:"jul",
+      month08:"aug",
+      month09:"sep",
+      month10:"oct",
+      month11:"nov",
+      month12:"dec"
+    },
+    ru: {
+      month01:"янв",
+      month02:"фев",
+      month03:"мар",
+      month04:"апр",
+      month05:"май",
+      month06:"июн",
+      month07:"июл",
+      month08:"авг",
+      month09:"сен",
+      month10:"окт",
+      month11:"ноя",
+      month12:"дек"
+    }
+   });
